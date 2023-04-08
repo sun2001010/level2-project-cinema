4f4f649c80dedcb778d9ef09419fe9f802b927c4
sun2001010 <1608163445@qq.com>
2023-04-08 11:23:58 +0800

    第一次提交

---
+++ cinema-web/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 80,
+    open: true,
+    // 代理,用来解决跨域问题
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        // 将路径中的/api重写为空 : /api/admin/login => /admin/login
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
